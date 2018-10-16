b02fd999dd2a53f247e5f996f20e603560669704
Danilo Woznica <danilowoz@gmail.com>
2018-10-16 10:28:35 +0100

    feat(Flow): Apply

--- src/createReducer/index.js
+++ src/createReducer/index.js
@@ -1,13 +1,16 @@
 // @flow
 
-type InitialType = {} | number | null
-type HandlersType = { [string]: () => InitialType }
-type ActionType = { type: string, error: boolean }
+type InitialType = {} | number | null;
+type ErrorType = boolean;
+type HandlersType = { [string]: (any, ActionType, ErrorType) => InitialType };
+type ActionType = { type: string, error: ErrorType };
 
 export default (initialState: InitialType, handlers: HandlersType) => (
   state: InitialType = initialState,
   action: ActionType
 ) => {
-  const reduceFn = handlers[action.type]
-  return reduceFn ? reduceFn((state: InitialType), action, action.error) : state
-}
+  const reduceFn = handlers[action.type];
+  return reduceFn
+    ? reduceFn((state: InitialType), (action: ActionType), action.error)
+    : state;
+};
