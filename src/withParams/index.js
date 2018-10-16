f19a17bcdbe21b03b3094d6966c5644edf89c432
Danilo Woznica <danilowoz@gmail.com>
2018-10-16 09:58:58 +0100

    fix(withParams): merge current params in the hoc

--- src/withParams/index.js
+++ src/withParams/index.js
@@ -1,76 +1,80 @@
-import React, { PureComponent } from "react"
-import queryString from "qs"
-import objEqual from "deep-equal"
+import React, { PureComponent } from "react";
+import queryString from "qs";
+import objEqual from "deep-equal";
 
-import getDisplayName from "../common/getDisplayName"
+import getDisplayName from "../common/getDisplayName";
 
 export default initialQuery => WrappedComponent => {
   if (!initialQuery) {
-    throw new Error("withParams requires an initial query.")
+    throw new Error("withParams requires an initial query.");
   }
 
   return class hocComponent extends PureComponent {
-    static displayName = `withParams(${getDisplayName(WrappedComponent)})`
+    static displayName = `withParams(${getDisplayName(WrappedComponent)})`;
 
     componentDidMount() {
-      this.setInitialParams()
+      this.setInitialParams();
     }
 
     componentDidUpdate() {
-      const { history } = this.props
+      const { history } = this.props;
 
       if (!history.location.search) {
-        this.setInitialParams()
+        this.setInitialParams();
       }
     }
 
     setInitialParams = () => {
-      const { history } = this.props
+      const { history } = this.props;
 
-      const currentParams = this.getCurrentSearch()
+      const currentParams = this.getCurrentSearch();
       const objSearch = {
         ...initialQuery,
         ...currentParams
-      }
-      const strSearch = queryString.stringify(objSearch)
+      };
+      const strSearch = queryString.stringify(objSearch);
       const searchObj = queryString.parse(history.location.search, {
         ignoreQueryPrefix: true
-      })
+      });
 
       if (!objEqual(searchObj, objSearch)) {
         history.replace({
           search: strSearch
-        })
+        });
       }
-    }
+    };
 
     getCurrentSearch = () => {
-      const { history } = this.props
-      const { search } = history.location
+      const { history } = this.props;
+      const { search } = history.location;
 
-      return queryString.parse(search, { ignoreQueryPrefix: true })
-    }
+      return queryString.parse(search, { ignoreQueryPrefix: true });
+    };
 
     updateParams = newParams => {
-      const { history } = this.props
+      const { history } = this.props;
+      const strSearch = queryString.stringify({
+        ...this.getCurrentSearch(),
+        ...newParams
+      });
 
       return history.replace({
         ...history.location,
-        search: queryString.stringify(newParams)
-      })
-    }
+        search: strSearch
+      });
+    };
 
     render() {
-      const { history } = this.props
-      const searchStr = history.location.search
-      const params = queryString.parse(searchStr, { ignoreQueryPrefix: true })
+      const { history } = this.props;
+      const searchStr = history.location.search;
+      const params = queryString.parse(searchStr, { ignoreQueryPrefix: true });
       return (
         <WrappedComponent
           params={params}
           updateParams={this.updateParams}
           {...this.props}
         />
-      )
+      );
     }
-  }
-}
+  };
+};
